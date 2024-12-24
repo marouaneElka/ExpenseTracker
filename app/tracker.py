baefd84303941aca2c247f2f95c7fb25082f3310
marouaneElka <marouane.elkaddouri@student.vives.be>
2024-12-24 22:46:56 +0100

    Navigation for expense tracker options

---
+++ app/tracker.py
@@ -0,0 +1,10 @@
+def run_menu(self):
+        while True:
+            print("\nPersonal Expense Tracker")
+            print("1. View All Expenses")
+            print("2. Add an Expense")
+            print("3. View Total Expenses by Category")
+            print("4. Remove an Expense")
+            print("5. Generate Report")
+            print("6. Exit")
+            choice = input("Enter your choice: ")
