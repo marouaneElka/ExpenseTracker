cf87a110ca38cd5b9e7bbf7cc5592b4593263a55
marouaneElka <marouane.elkaddouri@student.vives.be>
2024-12-24 23:33:04 +0100

    Updated ExpenseTracker: Added an add_expense and view expenses method

--- app/tracker.py
+++ app/tracker.py
@@ -6,6 +6,20 @@ class ExpenseTracker:
     def __init__(self):
         self.db_manager = DatabaseManager()
 
+    def add_expense(self, date, category, description, amount):
+        expense = Expense(date, category, description, amount)
+        self.db_manager.insert_expense(expense)
+        print("Expense added successfully.")
+
+    def view_expenses(self):
+        expenses = self.db_manager.fetch_expenses()
+        if expenses:
+            print("\nExpenses:")
+            for i, entry in enumerate(expenses, 1):
+                print(f"{i}. {entry['expense'].get_summary()}")
+        else:
+            print("No expenses found.")
+            
     def run_menu(self):
         while True:
             print("\nPersonal Expense Tracker")
